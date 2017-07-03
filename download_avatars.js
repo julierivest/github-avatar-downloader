ae5b36107f6f534764e9aa50996dfdecaaaacd18
Julie Rivest <julierivest93@gmail.com>
2017-07-03 19:06:56 +0000

    adding changes to download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,22 @@
 var request = require('request');
 
+var GITHUB_USER = "Yjulierivest";
+var GITHUB_TOKEN = "44a05436bb2e0a3fc2957a380376abe46655485c";
+
+
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  console.log(requestURL);
   // ...
 }
+
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
