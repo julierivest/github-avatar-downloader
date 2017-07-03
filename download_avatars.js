7a096cd3cd7c7f3ce6ab86e2abe7200238d53f0c
Julie Rivest <julierivest93@gmail.com>
2017-07-03 22:49:06 +0000

    adding FINAL final changes

--- download_avatars.js
+++ download_avatars.js
@@ -4,67 +4,54 @@ var fs = require('fs');
 var GITHUB_USER = "julierivest";
 var GITHUB_TOKEN = "44a05436bb2e0a3fc2957a380376abe46655485c";
 
-
-
+//----------FUNCTIONS---------------
 
 function getRepoContributors(cb) {
   var args = process.argv.slice(2);
     if (!args[0] || !args[1]) {
       throw new Error("MISSING ARGUMENT/S");
     }
-
-
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + args[0] + '/' + args[1] + '/contributors';
-
   var options = {
-  url: requestURL,
-  headers: {
-    'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
   }
-  }
-
-
   request(options, function(err, response, body) {
-
     if (err) {
       console.log("Error fetching");
     }
-
     if (response.statusCode === 200) {
       var json = JSON.parse(body);
-        cb(json);
+      cb(json);
     }
-
   })
-
 }
 
-
+//---------
 
 function printURLs(jsonObject) {
-
   for (var i = 0; i < jsonObject.length; i++) {
     var avatarURLs = jsonObject[i].avatar_url;
     downloadImageByURL(avatarURLs, `./avatars/${jsonObject[i].login}.jpg`);
   }
 }
 
+//----------
 
 function downloadImageByURL(url, filePath) {
   var options = {
-  url: url,
-  headers: {
-    'User-Agent': 'GitHub Avatar Downloader - Student Project'
-  }
+    url: url,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
   }
   request.get(options)
          .on('response', function(response){
          response.pipe(fs.createWriteStream(filePath));
     })
 }
-
-
-
 
 
 getRepoContributors(printURLs);
