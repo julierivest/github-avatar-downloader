23382c5a09a91536f86ca031546e7693b0cc379e
Julie Rivest <julierivest93@gmail.com>
2017-07-03 20:11:49 +0000

    adding changes to download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -8,15 +8,25 @@ var GITHUB_TOKEN = "44a05436bb2e0a3fc2957a380376abe46655485c";
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  //console.log(requestURL);
 
-  console.log(requestURL);
-  // ...
+  var options = {
+  url: requestURL,
+  headers: {
+    'User-Agent': 'GitHub Avatar Downloader - Student Project'
+  }
+};
+
+  request(options, cb);
+
+
 }
 
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  console.log("Result:", result.body);
+
 });
 
